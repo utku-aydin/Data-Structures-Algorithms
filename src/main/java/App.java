6ff5fbbaac97f7d804c1b1b9c3adeedad455d972
Utku Aydin <30289497+Brodican@users.noreply.github.com>
2020-09-26 15:31:08 +0300

    Sudoku solver built

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,50 @@
 import algorithms.BinarySearch;
 import algorithms.Fibonacci;
 import algorithms.GridPaths;
+import algorithms.SudokuSolver;
 
 public class App {
 
     public static void main(String[] args) {
-        /*BinarySearch binarySearch = new BinarySearch();
+/*        BinarySearch binarySearch = new BinarySearch();
         int[] testArr = {0,1,2,3,4,5,6,7,8,9};
-        System.out.println("Result: " + binarySearch.binarySearch(2, testArr));
+        System.out.println("Result: " + binarySearch.binarySearch(2, testArr));*/
 
-        Fibonacci fib = new Fibonacci();
+/*        Fibonacci fib = new Fibonacci();
         int n = 10;
         int[] mem = new int[n + 1];
         System.out.println("Fib of " + n + ": " + fib.getFibOf(n, mem));*/
 
-        GridPaths paths = new GridPaths();
-        int[][] grid = {{0,0,0},{0,0,0},{0,1,0}};
-        System.out.println(paths.pathCount(grid));
+/*        GridPaths paths = new GridPaths();
+        int[][] grid = {{0, 0, 0}, {0, 0, 0}, {0, 1, 0}};
+        System.out.println(paths.pathCount(grid));*/
+
+        SudokuSolver sudokuSolver = new SudokuSolver();
+        int[][] sudokuBoard =
+                {
+                        {1, 0, 2, 0, 9, 0, 0, 0, 0},
+                        {5, 0, 0, 0, 0, 7, 3, 1, 8},
+                        {4, 3, 0, 8, 0, 0, 2, 5, 9},
+                        {0, 1, 0, 7, 8, 0, 0, 0, 0},
+                        {0, 0, 0, 6, 4, 0, 9, 0, 0},
+                        {0, 0, 9, 3, 0, 5, 7, 2, 4},
+                        {0, 0, 0, 0, 3, 6, 1, 0, 0},
+                        {0, 0, 1, 0, 7, 8, 5, 0, 0},
+                        {9, 0, 0, 0, 0, 0, 8, 0, 7}
+                };
+        System.out.println("Before: ");
+        print2dArr(sudokuBoard);
+        System.out.println("Is board solvable? " + sudokuSolver.solveSudoku(sudokuBoard) + "\n");
+        print2dArr(sudokuBoard);
+    }
+
+    public static void print2dArr(int[][] toPrint) {
+        for (int[] ints : toPrint) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
---
+++ src/main/java/algorithms/SudokuSolver.java
@@ -0,0 +1,96 @@
+package algorithms;
+
+import java.util.HashMap;
+
+public class SudokuSolver {
+
+    public boolean solveSudoku(int[][] board) {
+        return solve(board, 0, 0);
+    }
+
+    private boolean solve(int[][] board, int row, int column) {
+        if (column == board.length) {
+            if (row == board.length - 1) {
+                //print2dArr(board);
+                return true;
+            }
+            return solve(board, row + 1, 0);
+        }
+
+        if (board[row][column] == 0) {
+            for (int i = 1; i < 10; i++) {
+                board[row][column] = i;
+                if (validateRow(board[row]) && validateColumn(getColumn(board, column)) && validateSquare(board, row, column)) {
+                    if (solve(board, row, column + 1))
+                        return true;
+                }
+            }
+            board[row][column] = 0;
+            return false;
+        } else {
+            return solve(board, row, column + 1);
+        }
+    }
+
+    private boolean validateRow(int[] row) {
+        HashMap<Integer, Integer> checker = new HashMap<>();
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] == 0)
+                continue;
+            if (checker.containsValue(row[i]))
+                return false;
+            checker.put(i, row[i]);
+        }
+
+        return true;
+    }
+
+    private boolean validateColumn(int[] column) {
+        HashMap<Integer, Integer> checker = new HashMap<>();
+        for (int i = 0; i < column.length; i++) {
+            if (column[i] == 0)
+                continue;
+            if (checker.containsValue(column[i]))
+                return false;
+            checker.put(i, column[i]);
+        }
+
+        return true;
+    }
+
+    private boolean validateSquare(int[][] board, int row, int column) {
+        int i = (row + 1) - (row) % 3 - 1;
+        int j = (column + 1) - (column) % 3 - 1;
+        HashMap<Integer, Integer> checker = new HashMap<>();
+        for (int n = i; n < i + 3; n++) {
+            for (int m = j; m < j + 3; m++) {
+                if (board[n][m] == 0)
+                    continue;
+                if (checker.containsValue(board[n][m]))
+                    return false;
+                checker.put(i, board[n][m]);
+            }
+        }
+
+        return true;
+    }
+
+    private int[] getColumn(int[][] board, int column) {
+        int[] col = new int[board.length];
+
+        for (int i = 0; i < board.length; i++) {
+            col[i] = board[i][column];
+        }
+
+        return col;
+    }
+
+    private void print2dArr(int[][] toPrint) {
+        for (int[] ints : toPrint) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+    }
+}
